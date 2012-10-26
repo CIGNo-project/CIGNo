7866981ba1be1fde6b4a23741a5f4f0d8fa5efe0
Stefano Menegon <ste.menegon@gmail.com>
2012-10-26 13:46:53 +0200

    adjust configuration

--- cigno/settings.py
+++ cigno/settings.py
@@ -10,17 +10,24 @@ ROOT_URLCONF = 'cigno.urls'
 
 # required by django-admin-tools
 TEMPLATE_CONTEXT_PROCESSORS = (
-    "django.core.context_processors.auth",
+    'django.contrib.auth.context_processors.auth',
+    'django.contrib.messages.context_processors.messages',
     "django.core.context_processors.debug",
     "django.core.context_processors.i18n",
     "django.core.context_processors.media",
+    "django.core.context_processors.request",
     "geonode.maps.context_processors.resource_urls",
-    # django 1.2 only
-    'django.contrib.messages.context_processors.messages',
-    # required by django-admin-tools
-    'django.core.context_processors.request',
-    # CIGNo
-    "cigno.context_processors.resource_urls",
+    # "django.core.context_processors.auth",
+    # "django.core.context_processors.debug",
+    # "django.core.context_processors.i18n",
+    # "django.core.context_processors.media",
+    # "geonode.maps.context_processors.resource_urls",
+    # # django 1.2 only
+    # 'django.contrib.messages.context_processors.messages',
+    # # required by django-admin-tools
+    # 'django.core.context_processors.request',
+    # # CIGNo
+    # "cigno.context_processors.resource_urls",
     )
 
 INSTALLED_APPS = (
@@ -36,11 +43,18 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.admin',
     'django.contrib.sitemaps',
-    'staticfiles',
+    'django.contrib.staticfiles',
+    'django.contrib.messages',
+
     'django_extensions',
     'registration',
     'profiles',
     'avatar',
+    'dialogos',
+    'agon_ratings',
+    'taggit',
+    'south',
+
     'geonode.core',
     'geonode.maps',
     'geonode.proxy',
@@ -52,7 +66,9 @@ INSTALLED_APPS = (
     'cigno.mdtools',
     'cigno',
     'modeltranslation',
-    'rosetta'
+    'rosetta',
+    'mediagenerator',
+    'cigno.aisstat',
 )
 
 MODELTRANSLATION_TRANSLATION_REGISTRY = "cigno.translation"
@@ -81,4 +97,47 @@ try:
     from local_settings import *
 except ImportError:
     pass
+
+# mediagenerator
+import sys
+sys.path.insert(0, os.path.join(os.path.dirname(__file__)))
+
+
+ROOT_MEDIA_FILTERS = {
+    'js': 'mediagenerator.filters.yuicompressor.YUICompressor',
+    'css': 'mediagenerator.filters.yuicompressor.YUICompressor',
+}
+
+YUICOMPRESSOR_PATH = '/usr/share/yui-compressor/yui-compressor.jar'
+
+MEDIA_DEV_MODE = False # DEBUG
+DEV_MEDIA_URL = '/static/cigno/'
+PRODUCTION_MEDIA_URL = '/static/cigno/'
+
+#GLOBAL_MEDIA_DIRS = (os.path.join(os.path.dirname(__file__), 'static'),)
+
+MEDIA_BUNDLES = (
+    ('cigno.js',
+     'cigno/externals/dynform/Ext.form.Field.js',
+     'cigno/externals/dynform/Ext.form.FieldSet.js',
+     'cigno/externals/dynform/Ext.form.FormPanel.js',
+     'cigno/externals/ext/examples/ux/Spinner.js',
+     'cigno/externals/ext/examples/ux/SpinnerField.js',
+     'cigno/externals/ext/examples/ux/MultiSelect.js',
+     'cigno/externals/ext/examples/ux/ItemSelector.js',
+     'cigno/externals/gemetclient/build/GemetClient.js',
+     'cigno/js/GemetPanel.js',
+     'cigno/externals/GeoExt.ux/GeoNamesSearchCombo.js',
+     'cigno/js/BboxGeoNamesPanel.js',
+     'cigno/js/RelationsManager.js',
+     'cigno/js/ResourceForm.js',
+     'cigno/externals/jit/js/jit-yc.js',
+     'cigno/js/SOSClient.js'
+    ),
+    ('cigno.css',
+     'cigno/externals/ext/examples/ux/css/Spinner.css',
+     'cigno/externals/ext/examples/ux/css/MultiSelect.css'
+     )
+
+)
 
